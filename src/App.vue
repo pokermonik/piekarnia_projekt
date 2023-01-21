9b39e1a26a5aefcf06ee71b484bb407ba176a84d
Mateusz Weclawski <pokermonik200@gmail.com>
2023-01-21 19:57:34 +0100

    przed pobraniem od Bartka

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <breads />
+    <reviews />
+  </div>
+  
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Breads from './components/Breads.vue'
+import reviews from './components/reviews.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
